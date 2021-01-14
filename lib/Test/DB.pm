30bec588e8a5b1d376de1b51aa449fe048c606ac
Al Newkirk <al@iamalnewkirk.com>
2021-01-13 22:49:05 -0500

    Fix all key/value pair unpacking method signatures

--- lib/Test/DB.pm
+++ lib/Test/DB.pm
@@ -15,64 +15,37 @@ use Data::Object::Class;
 # METHODS
 
 method build(Str :$database = $ENV{TESTDB_DATABASE}, Str %options) {
-  delete $options{database};
-
   if (lc($database) eq 'mssql') {
-    require Test::DB::Mssql;
-
-    my $generator = Test::DB::Mssql->new(%options);
-
-    return $generator;
+    require Test::DB::Mssql; return Test::DB::Mssql->new(%options);
   }
   elsif (lc($database) eq 'mysql') {
-    require Test::DB::Mysql;
-
-    my $generator = Test::DB::Mysql->new(%options);
-
-    return $generator;
+    require Test::DB::Mysql; return Test::DB::Mysql->new(%options);
   }
   elsif (lc($database) eq 'postgres') {
-    require Test::DB::Postgres;
-
-    my $generator = Test::DB::Postgres->new(%options);
-
-    return $generator;
+    require Test::DB::Postgres; return Test::DB::Postgres->new(%options);
   }
   elsif (lc($database) eq 'sqlite') {
-    require Test::DB::Sqlite;
-
-    my $generator = Test::DB::Sqlite->new(%options);
-
-    return $generator;
+    require Test::DB::Sqlite; return Test::DB::Sqlite->new(%options);
   }
   else {
-
     return undef;
   }
 }
 
 method create(Str :$database = $ENV{TESTDB_DATABASE}, Str %options) {
-  my $generator = $self->build(%options);
-
-  if ($generator) {
-
+  if (my $generator = $self->build(%options, database => $database)) {
     return $generator->create;
   }
   else {
-
     return undef;
   }
 }
 
 method clone(Str :$database = $ENV{TESTDB_DATABASE}, Str %options) {
-  my $generator = $self->build(%options);
-
-  if ($generator) {
-
+  if (my $generator = $self->build(%options, database => $database)) {
     return $generator->clone;
   }
   else {
-
     return undef;
   }
 }
